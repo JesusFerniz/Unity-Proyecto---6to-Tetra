ae0d352fb0d2326aded097d5b30c61faa22b81b8
Jazmin <ijazmin98@hotmail.com>
2021-05-21 12:55:43 -0500

    Scripts de vida, power vida y moneda

---
+++ Assets/Scenes/Jazmin/PowerUpVida.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpVida : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        VidaJugador health = FindObjectOfType<VidaJugador>();
+        health.hpUP();
+        Destroy(this.gameObject);
+
+    }
+}
---
+++ Assets/Scenes/Jazmin/VidaJugador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VidaJugador : MonoBehaviour
+{
+    public int vidas = 3;
+
+    public void hpUP()
+    {
+        if (vidas < 3)
+        {
+            vidas += 1;
+           
+        }
+
+    }
+}
