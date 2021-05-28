ea6d1b3ca2dcc7e59e739d27bfc6bbb301ab015e
Jazmin <ijazmin98@hotmail.com>
2021-05-28 00:56:27 -0500

    Moneda y Corazoncito

--- Assets/Scenes/Jazmin/PowerUpVida.cs
+++ Assets/Scenes/Jazmin/PowerUpVida.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PowerUpVida : MonoBehaviour
 {
+
+    private void Update()
+    {
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, rotationSpeed * Time.deltaTime, 0f));
+
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         VidaJugador health = FindObjectOfType<VidaJugador>();
