da66ef89bcca8242b93572b176a8ce3083228bb2
Luis Valdez <lukyva00@hotmail.com>
2021-08-12 17:17:11 -0500

    UI corazones y paredes para que no se caiga el player

--- Assets/Scenes/Jazmin/VidaJugador.cs
+++ Assets/Scenes/Jazmin/VidaJugador.cs
@@ -7,14 +7,30 @@ public class VidaJugador : MonoBehaviour
     public float vidasMax = 3f;
     public float currentHP;
     public GameObject canvasObject;
+    public GameObject[] hearts;
     private void Start()
     {
         currentHP = vidasMax;
 
     }
-
+    void Update()
+    {
+        if (currentHP < 1)
+        {
+            Destroy(hearts[0].gameObject);
+        }
+        if (currentHP < 2)
+        {
+            Destroy(hearts[1].gameObject);
+        }
+        if (currentHP < 3)
+        {
+            Destroy(hearts[2].gameObject);
+        }
+    }
     public void Damage(float amount)
     {
+
         currentHP -= amount;
         if(currentHP <= 0f)
         {
